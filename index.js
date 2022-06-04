77e88105774fa1532853a3c737d28659a723b877
김인후 <pino@gim-inhuui-MacBookPro.local>
2022-06-04 10:47:17 +0900

    12

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express"),
   app = express(),
   bodyParser = require("body-parser"),
   api = require("./api"),
-  pool = require("./config/database")
+  pool = require("./config/database"),
+  cors = require('cors')
 
+  app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use("/api", api)
