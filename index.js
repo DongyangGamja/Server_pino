018fe9e786550d400d2a158c15ca78bdf8e7f0e0
김인후 <pino@gim-inhuui-MacBookPro.local>
2022-06-04 10:08:03 +0900

    study

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require("express"),
   app = express(),
   bodyParser = require("body-parser"),
-  api = require("./api")
+  api = require("./api"),
+  pool = require("./config/database")
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use("/api", api)
+app.get("/test/:id", (req,res) => {
+  const param = req.params.id
+  pool(conn => {
+    conn.query("insert into tbl_test value(0, ?)", param, (err, doc)=> {
+      err ? res.send({result : err}) : res.send({result : true, data : param})
+    })
+  })
+})
 app.get("/", (req, res) => res.send("SERVER ON"))
 
-const port = 8001
+const port = 8080
 app.listen(port, () => console.log(`SERVER ON PORT : ${port}`))
